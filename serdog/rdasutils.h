cf7ae094fb78a73a650cb15aa6565464daa66c24
Stephen Quoll <stephenquoll@gmail.com>
2025-08-14 13:45:36 +1000

    Changing char array to char pointer and malloc to assure str is heap allocated

--- serdog/rdasutils.h
+++ serdog/rdasutils.h
@@ -4,9 +4,10 @@
 
 char* rdas_byte2string(uint8_t* in,int len)
 {
-	char str[len+1];
+	char *str;
+	str = malloc(len+1);
 	memcpy(str, in, len);
-	str[len] = 0; // Null termination.
+	str[len] = '\0'; // Null termination.
 	return str;
 };
 
